92cadb1cb66c00e706b6097db627d29df47a95f8
Wiphawan Piapramong <gitwiphawan@gmail.com>
2022-06-29 11:03:10 +0700

    commit 29/06

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Logo from './components/Logo'
-import User from './components/User'
-import { Button } from 'react-native-web'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Logo from "./components/Logo";
+import User from "./components/User";
+import { Button } from "react-native-web";
 
 const App = () => {
   return (
     <View>
-    <Logo/>
-    <Button
-      title = 'Click Me'
-      color = 'skyblue'
-      />
-    <User/>
+      <Logo />
+      <User />
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- components/Logo.js
+++ components/Logo.js
@@ -1,32 +1,34 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View,Button } from "react-native";
 
 const Logo = () => {
-    const textLogo = "Thai-Nihi";
-    const isTH = false;
-    return (
-        <View>
-            <Text style={styles.TextLogo}>TNI</Text>
-            <Text>{textLogo}</Text>
-            {/* {
+  const textLogo = "Thai-Nihi";
+  const isTH = false;
+  const showData =() =>{
+    alert("Hello Button")
+  }
+  return (
+    <View>
+      <Text style={styles.TextLogo}>TNI</Text>
+      <Text>{textLogo}</Text>
+      {/* {
                 isTH && <Text>ภาษาไทย</Text>
             } */}
-            {/* use if/else or conditonal operator */}
-            {
-                isTH
-                ?(<Text>ภาษาไทย</Text>)
-                :(<Text>ภาษาอังกฤษ</Text>)
-            }
+      {/* use if/else or conditonal operator */}
+      {isTH ? <Text>ภาษาไทย</Text> : <Text>ภาษาอังกฤษ</Text>}
+      <Button title="Click Me"
+       onPress={showData}
+       color="skyblue" 
+       />
+    </View>
+  );
+};
 
-        </View>
-    )
-}
-
-export default Logo
+export default Logo;
 
 const styles = StyleSheet.create({
-    TextLogo:{
-        color:"pink",
-        fontSize: 60
-    }
-})
+  TextLogo: {
+    color: "pink",
+    fontSize: 60,
+  },
+});
