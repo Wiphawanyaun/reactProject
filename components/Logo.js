d00451547cb16b6c1b375486a03c873951f03ae3
Wiphawan Piapramong <gitwiphawan@gmail.com>
2022-06-29 09:43:26 +0700

    first commit

---
+++ components/Logo.js
@@ -0,0 +1,20 @@
+/*import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Logo = () => {
+    return (
+        <View>
+            <Text style={styles.textLogo}>TNI</Text>
+        </View>
+    )
+}
+
+export default Logo
+
+const styles = StyleSheet.create({
+    textLogo:{
+        color:"blue",
+        fontSize: 60
+    }
+})
+*/
